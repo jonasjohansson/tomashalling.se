668bef7213581ecf99af46a47defbb188306e20b
Jonas Johansson <jns.johansson@gmail.com>
2025-04-25 22:12:35 +0200

    update

--- README.md
+++
@@ -1,1 +0,0 @@
-# tomashalling.se
---
+++ assets/script.js
@@ -0,0 +1,29 @@
+const images = document.querySelectorAll(".background-carousel img");
+
+// Pick a random starting index
+let current = Math.floor(Math.random() * images.length);
+images[current].classList.add("active");
+
+// Function to go to the next image
+function nextImage() {
+  images[current].classList.remove("active");
+  current = (current + 1) % images.length;
+  images[current].classList.add("active");
+}
+
+// Start the timer **only after initial random image is shown**
+let interval = setTimeout(() => {
+  nextImage();
+  interval = setInterval(nextImage, 5000);
+}, 5000);
+
+// Also change on click or tap
+function userNextImage() {
+  clearTimeout(interval);
+  clearInterval(interval);
+  nextImage();
+  interval = setInterval(nextImage, 5000);
+}
+
+document.addEventListener("click", userNextImage);
+document.addEventListener("touchstart", userNextImage);
